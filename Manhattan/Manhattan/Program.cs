3314cf975fbe4556859463cb4d4b52c666acddbd
Joseph <pumphrey.joseph@gmail.com>
2019-01-18 11:18:08 -0800

    ReturnAllNeighborhoodsFilterNoDuplicatesOrSpaces(string path) Return all neighborhoods and filters out duplicate neighborhoods and spaces

--- Manhattan/Manhattan/Program.cs
+++ Manhattan/Manhattan/Program.cs
@@ -14,7 +14,8 @@ namespace Manhattan
         {
             string path = "../../../../data.json";
             //ReturnAllNeighborhoodsFilterNoNames(path);
-            ReturnAllNeighborhoodsFilterNoDuplicates(path);
+            //ReturnAllNeighborhoodsFilterNoDuplicates(path);
+            ReturnAllNeighborhoodsFilterNoDuplicatesOrSpaces(path);
         }
         /// <summary>
         /// Returns all neighborhoods
@@ -63,7 +64,7 @@ namespace Manhattan
         /// <summary>
         /// Return all neighborhoods and filters out duplicate neighborhoods
         /// </summary>
-        /// <param name="path"></param>
+        /// <param name="path">JSON path</param>
         static void ReturnAllNeighborhoodsFilterNoDuplicates(string path)
         {
             var allNeighborhoods = "";
@@ -76,14 +77,39 @@ namespace Manhattan
                                 in deserializedProduct.features
                                 group classes by classes.properties.neighborhood
                                 into noDuplicate
-                                select noDuplicate;
+                                select noDuplicate.Key;
                                
             foreach (var item in neighborhoods)
             {
-                Console.WriteLine(item.Key);
+                Console.WriteLine(item);
+            }
+        }
+
+        /// <summary>
+        /// Return all neighborhoods and filters out duplicate neighborhoods and spaces
+        /// </summary>
+        /// <param name="path">JSON path</param>
+        static void ReturnAllNeighborhoodsFilterNoDuplicatesOrSpaces(string path)
+        {
+            var allNeighborhoods = "";
+            using (StreamReader reader = File.OpenText(path))
+            {
+                allNeighborhoods = reader.ReadToEnd();
+            }
+            RootObject deserializedProduct = JsonConvert.DeserializeObject<RootObject>(allNeighborhoods);
+            var neighborhoods = from classes
+                                in deserializedProduct.features
+                                where classes.properties.neighborhood != ""
+                                group classes by classes.properties.neighborhood
+                                into noDuplicate
+                                select noDuplicate.Key;
+
+            foreach (var item in neighborhoods)
+            {
+                Console.WriteLine(item);
             }
         }
 
     }
-
 }
+
